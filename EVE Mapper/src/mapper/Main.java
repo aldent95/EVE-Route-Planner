6f6b7bd67d12b10b1eb59c10f44236a4e0e2385d
Alex Dent <aldent95@hotmail.com>
2014-09-19 23:10:37 +1200

    Initial commit of already existing code.
    Signed-off-by: Alex Dent <aldent95@hotmail.com>

---
+++ EVE Mapper/src/mapper/Main.java
@@ -0,0 +1,9 @@
+package mapper;
+
+
+public class Main {
+	public static void main(String[] args){
+		new Mapper();
+	}
+
+}
---
+++ EVE Mapper/src/mapper/Mapper.java
@@ -0,0 +1,15 @@
+package mapper;
+
+public class Mapper {
+	private GUI gui;
+	
+	public Mapper(){
+		gui = new GUI(this);
+	}
+
+	public void route(String s, String e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
