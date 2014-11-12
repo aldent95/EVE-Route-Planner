f9a92319b028953a09e2f7a4992f7c263a1c1146
Alex Dent <aldent95@hotmail.com>
2014-11-12 21:34:36 +1300

    Working on System and Main
    Started and Finished the initial version of System. I expect it to
    change during testing.
    Also started working on Main, added all the methods from planning and
    started working on load()

--- EVE Route Finder/Main.py
+++ EVE Route Finder/Main.py
@@ -1,17 +1,28 @@
 #!/usr/bin/python
-
-
-class Main:
-    finder;
-    systems;
-    def load():
-
-    def setupGUI():
-
-    def setupRouteFinder():
-
-    def findRoute():
+import os
 
     
-    
-    
+#finder;
+systems = [];
+def __int__():
+     load();
+def load():
+    os.chdir(os.path.dirname(sys.argv[0]));
+    os.chdir(os.path.join(os.getcwd(), "..", "File Converter", "src", "files"));
+    temp = os.getcwd();
+    print (temp);
+    sysFile = open("Systems.txt", 'r', 1);
+    temp = [x.strip('\n') for x in sysFile.readlines()];
+    sysFile.close();
+    return
+def setupGUI():
+    return
+def setupRouteFinder():
+    return
+def findRoute():
+    return
+
+
+if __name__ == "__main__":
+    import sys
+    __int__();
--- EVE Route Finder/System.py
+++ EVE Route Finder/System.py
@@ -2,4 +2,35 @@
 
 
 class System:
-    
+    __exits = [];
+    __gatePos = {};
+    __sysID;
+    __name;
+    __constellation;
+    __region;
+    def __int__(self, sysID, name, constellation, region):
+        self.sysID = __sysID;
+        self.name = __name;
+        self.constellation = __constellation;
+        self.region = __region;
+    def addExit(self, exit):
+        self.__exits.append(exit);
+    def addGatePos(self, gate, pos):
+        self.__gatePos[gate] = pos;
+    def getExit(self, exitid):
+        for exit in self.__exits:
+            if exit.getID() == exitid:
+                return exit;
+        return;
+    def getGatePos(self, gateID):
+        return self.__gatePos[gateID];
+    def getExits(self):
+        return self.__exits;
+    def getID(self):
+        return self.__sysID;
+    def getName(self):
+        return self.__name;
+    def getConstellation(self):
+        return self.__constellation;
+    def getRegion(self):
+        return self.__region;
