b93243c1a5e964a53a2b09e566439128e2df7665
Alex Dent <aldent95@hotmail.com>
2014-11-13 20:59:38 +1300

    More implementation in Main and System
    Implemented getDistance in System, which returns the distance between 2
    gates,
    Finished off load in Main so that it now loads gate data as well.

--- EVE Route Finder/Main.py
+++ EVE Route Finder/Main.py
@@ -4,23 +4,31 @@ from System import System
 
     
 #finder;
-systems = [];
-def __int__():
+systems = {};
+def __init__():
      load();
 def load():
     os.chdir(os.path.dirname(sys.argv[0]));
-    sysFile = open("Systems.txt", 'r');
     lines = [];
-    for line in sysFile:
-         lines.append(line.strip('\n'));
+    with open("Systems.txt", 'r') as sysFile:
+         for line in sysFile:
+              lines.append(line.strip('\n'));
     print (len(lines));
     for i in range(0,len(lines)-1):
          line = lines[i].split('\t');
          tempSys = System(line[0], line[1], line[2], line[3], line[4], line[5], line[6],line[7])
-         systems.append(tempSys);
+         systems[line[3]] = tempSys;
     sysFile.close();
-    for system in systems:
-         print(system.toString());
+    lines = [];
+    with open("Stargates.txt", 'r') as gatesFile:
+         for line in gatesFile:
+              lines.append(line.strip('\n'))
+    for i in range(0,len(lines)-1):
+         line = lines[i].split('\t');
+         tempSys = systems[line[4]];
+         tempSys.addExit(line[5]);
+         pos = [line[1], line[2], line[3]];
+         tempSys.addGatePos(line[5], pos);
     return
 def setupGUI():
     return
@@ -32,4 +40,4 @@ def findRoute():
 
 if __name__ == "__main__":
     import sys
-    __int__();
+    __init__();
--- EVE Route Finder/System.py
+++ EVE Route Finder/System.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+import math
 
 class System:
     
@@ -8,11 +8,19 @@ class System:
         self.__name = name;
         self.__constellation = constellation;
         self.__region = region;
+        x = convert(x,1);
+        y = convert(y,1);
+        z = convert(z,1);
         self.__pos = [x,y,z];
         self.__secutiry = security;
+        self.__exits = [];
+        self.__gatePos = {};
     def addExit(self, exit):
         self.__exits.append(exit);
     def addGatePos(self, gate, pos):
+        pos[0] = convert(pos[0],2);
+        pos[1] = convert(pos[1],2);
+        pos[2] = convert(pos[2],2);
         self.__gatePos[gate] = pos;
     def getExit(self, exitid):
         for exit in self.__exits:
@@ -31,6 +39,23 @@ class System:
         return self.__constellation;
     def getRegion(self):
         return self.__region;
-    def toString(self):
-        tempstr = self.__name + " " + self.__region;
-        return tempstr;
+    def getDistance(self, gate1, gate2):
+        pos1 = self.__gatePos[gate1];
+        pos2 = self.__gatePos[gate2];
+        x = math.pow((float(pos2[0])-float(pos1[0])),2);
+        y = math.pow((float(pos2[1])-float(pos1[1])),2);
+        z = math.pow((float(pos2[2])-float(pos1[2])),2);
+        distance = (math.sqrt(x+y+z))/1000;
+        return distance;
+def convert(num,conType):
+    if(conType == 1):
+        num = num.split('e+');
+    else:
+        num = num.split('E');
+    num[0] = float(num[0]);
+    if len(num) >1:
+        num[1] = float(num[1]);
+        num = num[0]*math.pow(10,num[1]);
+    else:
+        num = num[0];
+    return num;
