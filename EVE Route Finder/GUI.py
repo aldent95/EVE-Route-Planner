d80399e2e132d3142825dd92096d3e2b77ef29a2
Alex Dent <aldent95@users.noreply.github.com>
2014-11-25 09:52:04 +1300

    Commented GUI

--- EVE Route Finder/GUI.py
+++ EVE Route Finder/GUI.py
@@ -1,58 +1,65 @@
 #!/usr/bin/python
+#Allows compatibility with any version of Python by checking for both versions of Tkinter
 try:
     from Tkinter import *
 except ImportError:
     from tkinter import *
+#Imports the AutoCompleteEntry
 from AutoCompleteEntry import AutoCompleteEntry
 
 class UI(Tk):
     def initialize(self, namesList):
-        w = 500;
-        h = 500;
-        sw = self.winfo_screenwidth();
-        sh = self.winfo_screenheight();
-        x=(sw-w)/2;
-        y =(sh-h)/2;
-        self.update();
-        self.geometry('%dx%d+%d+%d' % (w,h,x,y));
-        self.minsize(int(w),int(h/2));
+    #Handles setting up most of the GUI
+        w = 500;#Window width
+        h = 500;#Window height
+        sw = self.winfo_screenwidth();#Gets screen width
+        sh = self.winfo_screenheight();#Gets screen height
+        x=(sw-w)/2;#Calculates the x position for the left side of the window that allows it to be placed in the center of the screen
+        y =(sh-h)/2;#Calculates the y position for the top of the window that allows it to be placed in the center of the screen
+        self.update();#Forces and update on the window
+        self.geometry('%dx%d+%d+%d' % (w,h,x,y));#Sets the windows width, height and position
+        self.minsize(int(w),int(h/2));#Sets the minimum size of the window
         
-        self.columnconfigure(0,weight=1);
+        self.columnconfigure(0,weight=1);#Configure all used columns to automaticly resize
         self.columnconfigure(1,weight=1);
         self.columnconfigure(2,weight=1);
         self.columnconfigure(3,weight=1);
-        self.rowconfigure(1,weight=1);
+        self.rowconfigure(1,weight=1);#Configures the row uesd for the text area to automaticly resize
         
-        self.title("EVE Route Finder");
-        self.grid();
-        self.configureMenu();
+        self.title("EVE Route Finder");#Sets the title
+        self.grid();#Sets the layout to use grid
+        self.configureMenu();#Calls the function that configures the menus
         
-        Label(self,padx=2,text="Origin").grid(row=0,column=0,sticky='E'+'W');
-        self.origin = AutoCompleteEntry(self, namesList);
-        self.origin.grid(column=1,row=0,sticky='E'+'W');
+        Label(self,padx=2,text="Origin").grid(row=0,column=0,sticky='E'+'W');#Setup and place the origin label
+        self.origin = AutoCompleteEntry(self, namesList);#Setup the origin autocomplete entry box
+        self.origin.grid(column=1,row=0,sticky='E'+'W');#Place the origin entry box
         
-        Label(self,padx=2,text="Destination").grid(row=0,column=2,sticky='E'+'W');
-        self.des = AutoCompleteEntry(self,namesList);
-        self.des.grid(column=3,row=0,sticky='E'+'W');
+        Label(self,padx=2,text="Destination").grid(row=0,column=2,sticky='E'+'W');#Setup and place the Destination label
+        self.des = AutoCompleteEntry(self,namesList);#Setup the destination autocomplete entry box
+        self.des.grid(column=3,row=0,sticky='E'+'W');#Place the destination entry box
         
-        self.output = Text(self);
-        self.output.grid(column=0,row=1,sticky='E'+'W'+'N'+'S',columnspan=4);
-        self.output.insert('end',"Output test");
-        self.output.configure(state='disabled');
+        self.output = Text(self);#Setup the text area
+        self.output.grid(column=0,row=1,sticky='E'+'W'+'N'+'S',columnspan=4);#Place the text area, spanning 4 columns
+        self.output.insert('end',"Output test");#Add some testing text to the text area
+        self.output.configure(state='disabled');#Disable the text area
         
     def configureMenu(self):
-        menu = Menu(self);
-        filemenu = Menu(menu,tearoff=0);
-        menu.add_cascade(label="File",menu=filemenu);
-        self.config(menu=menu);
+        #Handles configuring and setting up the menus
+        menu = Menu(self);#Setup the menu bar
+        filemenu = Menu(menu,tearoff=0);#Setup the filemenu
+        menu.add_cascade(label="File",menu=filemenu);#Place the filemenu as a cascading menu
+        self.config(menu=menu);#Configure the meun for the GUI to be the menu we just set up
         
     def setupButtons(self):
+        #Handles creating and setting up all the buttons (Not really in use yet)
         quitButton= Button(self, text="Quit", command=self.quit)
         quitButton.place(x=0,y=0);
     def quit(self):
+    #Handles quitting the program, not working yet
        return; 
     def __init__(self, parent, namesList):
-       Tk.__init__(self,parent);
-       self.parent = parent;
-       self.initialize(namesList);
-       self.mainloop();
+    #Handles the initial call to create a GUI
+       Tk.__init__(self,parent);#Parent constructor
+       self.parent = parent;#Store the parent
+       self.initialize(namesList);#Initilize the GUI
+       self.mainloop();#Start the main loop
