71e8b3152adbd8c0d2b6cb727160ce554a233790
Alex Dent <aldent95@hotmail.com>
2015-04-06 17:55:45 +1200

    Add comments to Main
    Fixed a problem where I had no comments in main. Never a good idea to
    not have comments, make sure you all ways have comments.
    
    Now to figure out exactly how Routes works since I didn't comment that
    and its been so long since I worked on it.

--- EVE Route Finder/Main.py
+++ EVE Route Finder/Main.py
@@ -8,32 +8,32 @@ from Routes import RouteFinder
 
 class Main:
      def load(self):
-         os.chdir(os.path.dirname(sys.argv[0]));
-         lines = [];
-         with open("Systems.txt", 'r') as sysFile:
-              for line in sysFile:
-                   lines.append(line.strip('\n'));
-         for i in range(0,len(lines)-1):
-              line = lines[i].split('\t');
-              tempSys = System(line[0], line[1], line[2], line[3], line[4], line[5], line[6],line[7])
-              self.systems[line[3]] = tempSys;
-              self.nameList.append(line[0]);
-              self.sysNames[line[0]] = line[3];
-         sysFile.close();
-         self.nameList= sorted(self.nameList);
-         lines = [];
-         with open("Stargates.txt", 'r') as gatesFile:
-              for line in gatesFile:
-                   lines.append(line.strip('\n'))
-         for i in range(0,len(lines)):
-              line = lines[i].split('\t');
-              tempSys = self.systems[line[4]];
-              tempSys.addadjSys(line[5]);
-              pos = [line[1], line[2], line[3]];
-              tempSys.addGatePos(line[5], pos);
+         os.chdir(os.path.dirname(sys.argv[0])); #Set the current directory to the correct one
+         lines = []; #Set up the lines array
+         with open("Systems.txt", 'r') as sysFile: #Open the systems file
+              for line in sysFile: #For each line in the file
+                   lines.append(line.strip('\n'));#Append the line to the array after stripping the new line char
+         for i in range(0,len(lines)-1): #For each entry in the array
+              line = lines[i].split('\t'); #Split by tabs
+              tempSys = System(line[0], line[1], line[2], line[3], line[4], line[5], line[6],line[7]) #Create a sys object
+              self.systems[line[3]] = tempSys; #Add the sys object to the systems dict, with the sys id as key
+              self.nameList.append(line[0]); #Append the sys name to the name list
+              self.sysNames[line[0]] = line[3]; #Add the sys name to the sysNames array, putting it in the entry corosponding to its id
+         sysFile.close(); #Close the file
+         self.nameList= sorted(self.nameList); #Sort the name list
+         lines = []; #Clear the lines array
+         with open("Stargates.txt", 'r') as gatesFile: #Open the gates file
+              for line in gatesFile: #For each line in gates file
+                   lines.append(line.strip('\n')) #Append to the lines array after stripping the new line char
+         for i in range(0,len(lines)): #For each entry in the array
+              line = lines[i].split('\t'); #Split by tabs
+              tempSys = self.systems[line[4]]; #Retrive the origin system
+              tempSys.addadjSys(line[5]); #Add the adjacent system
+              pos = [line[1], line[2], line[3]]; #Add the gate pos to an object
+              tempSys.addGatePos(line[5], pos); #Add the gate pos to the system
          return
      def setupUI(self):
-         self.gui = UI(None, self.nameList, self); 
+         self.gui = UI(None, self.nameList, self); #Set up the GUI, pass it the names list and the main object
          return
      def setupRouteFinder(self):
          #Get route type setting from UI
@@ -43,22 +43,22 @@ class Main:
           #Get specific security status filtering
           #Get Sov setting
           #Generate route finder based on settings
-          self.routeFinder = RouteFinder()
+          self.routeFinder = RouteFinder() #Setup the route finder
      def findRoute(self, origin, destination):
-          oriSys = self.systems[self.sysNames[origin]]
-          destSys = self.systems[self.sysNames[destination]]
-          self.routeFinder = RouteFinder(oriSys, destSys, self.systems)
-          route = self.routeFinder.getRoute()
+          oriSys = self.systems[self.sysNames[origin]] #Get the origin system
+          destSys = self.systems[self.sysNames[destination]] #Get the destination system
+          self.routeFinder = RouteFinder(oriSys, destSys, self.systems) #Set up the route finder **TEMP**
           #setupRouteFinder()
+          route = self.routeFinder.getRoute('j') #Get the route
           return route
      def __init__(self):
-          self.systems = {};
-          self.gui = "";
-          self.nameList = [];
-          self.sysNames={}
-          self.routeFinder = ""
-          self.load();
-          self.setupUI();
+          self.systems = {}; #Initilise the systems dict
+          self.gui = ""; #Initilise the gui object
+          self.nameList = [];#Initilise the name list
+          self.sysNames={}#Initilise the sysNames dict
+          self.routeFinder = "" #Initilise the routefinder object
+          self.load(); #Load the files
+          self.setupUI(); #Setup the gui
           
 
 if __name__ == "__main__":
